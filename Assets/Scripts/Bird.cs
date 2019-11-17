4dbf48d38f1e6dc4c9dcb34e3af50c8685b27ad9
tananw <wojtek.tanan@gmail.com>
2019-11-17 19:24:46 +0100

    Added player controlled bird, gave it ability to move

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movementSpeed;
+    public float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(movementSpeed, rb.velocity.y);
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+        if(transform.position.y > 18 || transform.position.y < -19)
+        {
+            Death();
+        }
+    }
+
+    public void Death()
+    {
+        rb.velocity = Vector3.zero;
+        transform.position = new Vector2(0, 0);
+    }
+}
