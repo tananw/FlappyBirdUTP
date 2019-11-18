ba292293bbf8f2d1dd86454226e36be14e0345eb
tananw <wojtek.tanan@gmail.com>
2019-11-18 19:01:18 +0100

    Fixed start menu

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,17 +7,24 @@ public class Bird : MonoBehaviour
 {
 	public float speed = 2;
 	public float tapForce = 300;
+    public GameObject target;
 
 	public UnityEngine.UI.Text text;
 	// Start is called before the first frame update
 	void Start() {
-		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        Time.timeScale = 0;
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
 	}
 
 	// Update is called once per frame
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
-			text.gameObject.SetActive(false);
+            if(target != null)
+            {
+                Destroy(target);
+            }
+            Time.timeScale = 1;
+            text.gameObject.SetActive(false);
 			GetComponent<Rigidbody2D>().AddForce(Vector2.up * tapForce);
 		}
 	}
