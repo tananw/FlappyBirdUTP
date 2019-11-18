f85f5c88da9ab83921a55976c56a191d73256939
Marcin Siuda <marcin.siuda@outlook.com>
2019-11-18 17:51:05 +0100

    Create bird and camera scripts

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed = 2;
+	public float tapForce = 300;
+	// Start is called before the first frame update
+	void Start() {
+		GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up * tapForce);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		Application.LoadLevel(Application.loadedLevel);
+	}
 }
---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+	public Transform target;
+
+
+	// Update is called once per frame
+	void LateUpdate() {
+		transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+	}
+}
