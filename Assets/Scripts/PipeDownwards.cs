4b54669032af884a97731ba59e815b518b83d531
tananw <wojtek.tanan@gmail.com>
2019-11-18 16:10:14 +0100

    Added background and ground with their physics, also pipe prefabs

--- Assets/Scripts/PipeDownwards.cs
+++ Assets/Scripts/PipeDownwards.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class PipeDownwards : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
