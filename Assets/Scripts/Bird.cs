3fd2c259323fa549ab9c59f568c9b96b38b29cee
tananw <wojtek.tanan@gmail.com>
2019-11-19 23:11:43 +0100

    Minor changes in physics, added upper limitation for bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -6,27 +6,28 @@ using UnityEngine.UI;
 
 public class Bird : MonoBehaviour
 {
-	public float speed = 2;
 	public float tapForce = 300;
 	public bool isDead = false;
 	public AudioSource jumpSound, bumpSound;
     private Rigidbody2D rigid2D;
+    private int screenHeight = Screen.height;
 
-    // Start is called before the first frame update
     void Start() {
 		Time.timeScale = 0;
         rigid2D = GetComponent<Rigidbody2D>();
-        //GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
     }
         
-	// Update is called once per frame
 	void Update() {
 		if (!isDead) {
 			if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
 				jumpSound.Play();
+                Time.timeScale = 1;
                 rigid2D.velocity = Vector2.zero;
-				Time.timeScale = 1;
-				rigid2D.AddForce(Vector2.up * tapForce);
+                rigid2D.AddForce(Vector2.up * tapForce);
+                if(rigid2D.position.y >= 4.5f)
+                {
+                    rigid2D.AddForce(Vector2.up * -tapForce);
+                }
 			}
 		}
 	}
